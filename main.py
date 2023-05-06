ff91ae7a6dee4b552b0dce96d5ef09630c11202b
monicapulipati09 <pulipatimonica@gmail.com>
2023-05-06 13:04:13 +0530

    change in code line to reduce params

--- main.py
+++ main.py
@@ -14,29 +14,36 @@
 # of (1)", don't actually divide or reduce the fraction to decimal
 
 
-def find_probability(number_of_missing_days, current_day, number_of_days_till_now, ways_to_attend, prob_of_miss, total_number_of_days):
-    """
 
-    :param number_of_missing_days: number of consecutive missing days, if present on a day, number of missig days will become 0
-    :param current_day: value will be '0' or '1' 0 - Absent, 1 - Present
-    :param number_of_days_till_now: Number of days completed till now.
-    :return:ways_to_attend  - total ways to attend , prob_of_miss - number of ways to miss grad day.
-    """
+def find_ways(total_number_of_days):
+    ways_to_attend = [0]
+    prob_of_miss = [0]
+    total_number_of_days = total_number_of_days
+    def find_probability(number_of_missing_days, current_day, number_of_days_till_now):
+        """
 
-    if number_of_missing_days >= 4:
-        return prob_of_miss, ways_to_attend
+        :param number_of_missing_days: number of consecutive missing days, if present on a day, number of missig days will become 0
+        :param current_day: value will be '0' or '1' 0 - Absent, 1 - Present
+        :param number_of_days_till_now: Number of days completed till now.
+        :return:ways_to_attend  - total ways to attend , prob_of_miss - number of ways to miss grad day.
+        """
 
-    if number_of_days_till_now == total_number_of_days:
-        ways_to_attend += 1
-        if current_day == '0':
-            prob_of_miss += 1
-        return prob_of_miss, ways_to_attend
+        if number_of_missing_days >= 4:
+            return
 
-    prob_of_miss, ways_to_attend = find_probability(number_of_missing_days+1, '0', number_of_days_till_now+1, ways_to_attend, prob_of_miss, total_number_of_days)
-    prob_of_miss, ways_to_attend = find_probability(0, '1', number_of_days_till_now+1, ways_to_attend, prob_of_miss, total_number_of_days)
+        if number_of_days_till_now == total_number_of_days:
+            ways_to_attend[0] += + 1
+            if current_day == '0':
+                prob_of_miss[0] += 1
+            return
 
-    return prob_of_miss, ways_to_attend
+        find_probability(number_of_missing_days+1, '0', number_of_days_till_now+1)
+        find_probability(0, '1', number_of_days_till_now+1)
+        return
+
+    find_probability(0, '0', 0)
+    print(str(prob_of_miss[0]) + "/" + str(ways_to_attend[0]))
 
 N = 5
-p, s = find_probability(0,'0',0, 0, 0, N)
-print(str(p)+ "/" +str(s))
+find_ways(N)
+
